2efc3fd5efa59fac5b2e8c9c1fcb64e724934ff6
Alexander Mai <alexandermai94@live.de>
2026-03-20 20:55:39 +0100

    add functionality for adding, editing and deleting Spells

--- spell-scroll/src/components/Scroll.jsx
+++ spell-scroll/src/components/Scroll.jsx
@@ -1,25 +1,65 @@
 import { useState } from 'react'
 import Spell from './Spell'
 
-function Scroll() {
-  const [inputValue, setinputValue] = useState("")
+const initialSpells = [
+  {id: 0, title: "Fireball"},
+  {id: 1, title: "Touchy Healy"},
+  {id: 2, title: "Yes, I Can"},
+]
 
-  function addSpellToList(){}
+function Scroll() {
+  const [spellArray, setSpellArray] = useState(initialSpells)
+  const [inputValue, setInputValue] = useState("")
+
+  function addSpellToArray(){
+    const newSpell = {
+      id: spellArray[spellArray.length-1].id +1,
+      title: inputValue
+    }
+    setSpellArray([...spellArray, newSpell])
+    setInputValue("")
+  }
+
+  // Callback function (??) fed brack from child
+  function removeSpell(id) {
+    setSpellArray(
+      spellArray.filter(a => a.id !== id)
+    )
+  }
+
+  // Callback function (??) fed brack from child
+  function updateSpell(data){
+    setSpellArray(spellArray.map(spell => {
+      if (spell.id === data.id) {
+        // Create a *new* object with changes
+        return { ...spell, title: data.title };
+      } else {
+        // No changes
+        return spell;
+      }
+    }));
+
+  }
   
 
   return (<section className='scroll'>
 
     <div className="spell-add">
-      <input type="text" onChange={event => setinputValue(event.target.value)} />
-      <button onClick={addSpellToList}>Add Spell</button>
+      <input type="text" onChange={event => setInputValue(event.target.value)} value={inputValue}/>
+      <button onClick={addSpellToArray}>Add Spell</button>
     </div>
     
     <hr />
 
     <ul className='spell-list'>
-      <Spell title="Fireball"/>
-      <Spell title="Touchy Healy"/>
-      <Spell title="Yes I Can"/>
+      {spellArray.map((element) => {
+        return <Spell 
+          key={element.id}
+          {...element}
+          updateSpell={updateSpell}
+          removeSpell={removeSpell}
+        />
+      })}
     </ul>
 
   </section>)
--- spell-scroll/src/components/Spell.jsx
+++ spell-scroll/src/components/Spell.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import { useState } from "react"
 
-function Spell({id, title}) {
+function Spell(props) {
+  const [editValue, setEditValue] = useState(props.title)
+  const [editMode, setEditMode] = useState(false)
 
-  return (<li key={id}>
+  if (!editMode) {
+    // NOT edit view
+    return (<li key={props.id} className='spell-item'>
 
-    <h3>{title}</h3>
-    
-    <div className="buttons">
-      <button>✏️</button>
-      <button>🗑️</button>
-    </div>
-  </li>)
+      <h3>{props.title}</h3>
+      
+      <div className="buttons">
+        <button onClick={() => setEditMode(true)}>✏️</button>
+        <button onClick={() => props.removeSpell(props.id)}>🗑️</button>
+      </div>
+    </li>)
+
+  } else {
+    // Edit view
+    // input value is prefilled with the initial text
+    return (<li className='spell-item'>
+
+      <h3>
+        <input type="text" 
+          onChange={event => setEditValue(event.target.value)} 
+          value={editValue}/>
+      </h3>
+      
+      <div className="buttons">
+        <button onClick={() => {
+          // send updated spell to parent
+          props.updateSpell({
+            id: props.id,
+            title: editValue
+          })
+          // toggle edit mode off
+          setEditMode(false)
+        }}>💾</button>
+        <button onClick={() => {
+          // reset edit value
+          setEditValue(props.title)
+          // toggle edit mode off
+          setEditMode(false)
+          }}>❌</button>
+      </div>
+    </li>)
+  }
 }
 
 export default Spell
