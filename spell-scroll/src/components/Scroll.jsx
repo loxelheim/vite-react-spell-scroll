db3d03a0e3f1a2cbbbc91beb8d7ebd29f3404e47
Alexander Mai <alexandermai94@live.de>
2026-03-20 18:21:00 +0100

    start basic overall structure for elements

---
+++ spell-scroll/src/components/Scroll.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Spell from './Spell'
+
+function Scroll() {
+
+  return (<>
+    <ul>
+      <Spell />
+    </ul>
+  </>)
+}
+
+export default Scroll
---
+++ spell-scroll/src/components/Spell.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Spell() {
+  return (<>
+    <li>Spell</li>
+  </>)
+}
+
+export default Spell
